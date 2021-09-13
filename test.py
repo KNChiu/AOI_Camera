e8cddad3e78a2521e03355ac37a8f6474bb0df6e
KNChiu <y9760210@gmail.com>
2021-09-13 18:44:58 +0800

    移除防呆

--- test.py
+++ test.py
@@ -2,97 +2,97 @@ import time
 import cv2
 from AOI_Camera import CameraAPI
 
-if __name__ == "__main__":
-    # CameraAPI = CameraAPI()
 
-    # CameraAPI.enum_devices()        # 列出可用相機
-    # print("enum_devices")
-    # time.sleep(0.1)
+AOICameraAPI = CameraAPI()
+AOICameraAPI.fast_gradding()
 
-    # CameraAPI.open_device()         # 開啟相機
-    # print("open_device")        
-    # time.sleep(0.5)
+cv2.namedWindow("showIMG",0)
+cv2.resizeWindow("showIMG", 500, 500) 
 
-    # CameraAPI.start_grabbing()      # 開始串流
-    # print("start_grabbing")
-    # time.sleep(0.5)
-
-    # 建立一個顯示畫面
-    # cv2.namedWindow("showIMG",0)
-    # cv2.resizeWindow("showIMG", 500, 500) 
-
-    while True:
-        AOICameraAPI = CameraAPI()
-
-        AOICameraAPI.enum_devices()        # 列出可用相機
-        print("enum_devices")
-        time.sleep(0.1)
-
-        AOICameraAPI.open_device()         # 開啟相機
-        print("open_device")        
-        time.sleep(0.5)
-
-        AOICameraAPI.start_grabbing()      # 開始串流
-        print("start_grabbing")
-        time.sleep(0.5)
-
-        AOICameraAPI.enum_devices()        # 列出可用相機
-        print("enum_devices")
-        time.sleep(0.1)
+while True:
+    numArray = AOICameraAPI.get_img_nummpy()
+    if numArray is None:
+        pass
+    else:
+        cv2.imshow("showIMG", numArray)
+        key = cv2.waitKey(1)
+        if key & 0xFF == ord('q'):
+            break
+        elif key & 0xFF == ord('s'):
+            AOICameraAPI.bmp_save(save_path = r"./CameraAPI/SaveBMP", save_name = "123")
+            print("bmp_save")
+            time.sleep(0.1)
 
 
-        AOICameraAPI.open_device()         # 開啟相機
-        print("open_device")        
-        time.sleep(0.5)
+AOICameraAPI.stop_grabbing()
+print("stop_grabbing")
+time.sleep(0.1)
 
-        AOICameraAPI.start_grabbing()      # 開始串流
-        print("start_grabbing")
-        time.sleep(0.5)
-        isConnect = True
+AOICameraAPI.close_device()
+print("close_device")
+time.sleep(0.1)
 
-        while True:
-            numArray = AOICameraAPI.get_img_nummpy()   # 取得影像的
-            # print(numArray)
-            time.sleep(0.5)
+print("finish")
+
+
+# if __name__ == "__main__":
+#     cnt = 0
+#     gradding = True
+
+#     while gradding:
+#         AOICameraAPI = CameraAPI()
+#         # AOICameraAPI.fast_gradding()
+
+#         AOICameraAPI.enum_devices()
+#         print("enum_devices")
+#         time.sleep(0.1)
+
+#         AOICameraAPI.open_device()
+#         print("open_device")
+#         time.sleep(0.5)
+
+#         AOICameraAPI.start_grabbing()
+#         print("start_grabbing")
+#         time.sleep(0.5)
+
+#         isConnect = True
+#         cv2.namedWindow("showIMG",0)
+#         cv2.resizeWindow("showIMG", 500, 500) 
+
+#         while gradding:
+#             numArray = AOICameraAPI.get_img_nummpy()   # 取得影像的
+#             # print(numArray)
+#             time.sleep(0.5)
             
+#             if numArray is None:
+#                 print("Camera disconnected")
+#                 if isConnect == False:
+#                     break
+#                 else:
+#                     time.sleep(3)
+#                     isConnect = False
+#                     continue
+#             else:
+#                 isConnect = True
 
-            if numArray is None:
-                print("None")
-                if isConnect == False:
-                    break
-                else:
-                    time.sleep(3)
-                    isConnect = False
-                    continue
-            else:
-                isConnect = True
-                print("not None")
+#                 cv2.imshow("showIMG", numArray)
+#                 key = cv2.waitKey(1)
+#                 if key & 0xFF == ord('q'):
+#                     gradding = False
+#                     break
+#                 elif key & 0xFF == ord('s'):
+#                     nameList = ['Tr_1_G', 'Tr_2_G', 'Tr_1_NG', 'Tr_2_NG']
+#                     AOICameraAPI.bmp_save(save_path = r"SaveBMP", save_name = nameList[cnt])   # 將影像存成BMP檔案
+#                     cnt += 1
+#                     print("bmp_save")
+#                     time.sleep(0.1)
+#     else:
+#         cv2.destroyAllWindows()             # 釋放
 
-    # cnt = 0
-    # while True:
-        # if numArray is None:
-        #     pass
-        # else:
-        #     cv2.imshow("showIMG", numArray)
-        #     key = cv2.waitKey(1)
-        #     if key & 0xFF == ord('q'):
-                
-        #         break
-        #     elif key & 0xFF == ord('s'):
-        #         nameList = ['Tr_1_G', 'Tr_2_G', 'Tr_1_NG', 'Tr_2_NG']
-        #         CameraAPI.bmp_save(save_path = r"SaveBMP", save_name = nameList[cnt])   # 將影像存成BMP檔案
-        #         cnt += 1
-        #         print("bmp_save")
-        #         time.sleep(0.1)
+#         AOICameraAPI.stop_grabbing()           # 停止串流
+#         time.sleep(0.1)
 
-    cv2.destroyAllWindows()             # 釋放
-
-    CameraAPI.stop_grabbing()           # 停止串流
-    print("stop_grabbing")
-    time.sleep(0.1)
-
-    CameraAPI.close_device()            # 關閉設備
-    print("close_device")
-    time.sleep(0.1)
-
-    print("finish")
+#         AOICameraAPI.close_device()            # 關閉設備
+#         time.sleep(0.1)
+#     print(gradding)
+#     print("finish")
